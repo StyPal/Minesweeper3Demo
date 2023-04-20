1be695680b1d3565ab4facbb98dc752ca249944b
StyPal <126196078+StyPal@users.noreply.github.com>
2023-04-20 08:23:06 +0200

    Type::value + set/get added

--- temp/src/main/kotlin/Type.kt
+++ temp/src/main/kotlin/Type.kt
@@ -1,11 +1,18 @@
 enum class Type {
     EMPTY,
     MINE,
-    NEAR_MINE {
-        private var value: Int = 0;
-        fun add(){
-            value++
-        }
-    };
-}
+    NEAR_MINE;
 
+    private var value = 0
+    fun setValue(value: Int) {
+        if (this == NEAR_MINE) {
+            this.value = value
+        } else {
+            this.value = 0
+        }
+    }
+
+    fun getValue(): Int {
+        return this.value
+    }
+}
