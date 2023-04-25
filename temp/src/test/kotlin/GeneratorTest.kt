e182240260a92b11f21ba4b17007815ebcee5b4b
StyPal <126196078+StyPal@users.noreply.github.com>
2023-04-25 18:05:46 +0200

    updated GeneratorTest

--- temp/src/test/kotlin/GeneratorTest.kt
+++ temp/src/test/kotlin/GeneratorTest.kt
@@ -5,7 +5,7 @@ import org.junit.jupiter.api.Test
 class GeneratorTest {
 
     private lateinit var generator: Generator
-    private lateinit var plane: Array<Array<Type?>>
+    private lateinit var plane: Array<Array<Field?>>
     private val siteLength = 3
     private val mineCount = 2
 
@@ -16,32 +16,45 @@ class GeneratorTest {
         }
         plane.forEachIndexed { r, row ->
             row.forEachIndexed { c, _ ->
-                plane[r][c] = Type.MINE
+                plane[r][c] = Field()
             }
         }
         generator = Generator(plane)
     }
 
+    private fun getResult(): ArrayList<Position>{
+        return generator.generateMines(3, siteLength * siteLength - mineCount)
+    }
+
     @Test
-    fun generateMines() {
-        val result = generator.generateMines(3, siteLength * siteLength - mineCount)
+    fun generateMines_size() {
+        val result = getResult()
         // Check if the size of the ArrayList of the Mine Positions is the same as mineCount
         assertEquals(result.size, mineCount)
+    }
 
+    @Test
+    fun generateMines_contentOfArrayList(){
+        val result = getResult()
         // Iterates through the ArrayList of the mine positions
         result.forEach { pos ->
-            if (plane[pos.row][pos.col] != Type.MINE) {
+            if (plane[pos.row][pos.col]?.type != Type.MINE) {
                 fail()
             } else {
                 assertTrue(true)
             }
         }
 
+    }
+
+    @Test
+    fun generateMines_temp(){
+        val result = getResult()
         // Checks if there is a mine is not in the ArrayList
         plane.forEachIndexed { r, row ->
             row.forEachIndexed { c, _ ->
                 var b = false
-                if (plane[r][c] == Type.MINE) {
+                if (plane[r][c]?.type == Type.MINE) {
                     result.forEach { positions ->
                         if (positions.row == r && positions.col == c) {
                             b = true
